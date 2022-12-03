19e1c720cc58b841605b9991323eed746e53b1ae
aciq <36763595+aciq@users.noreply.github.com>
2022-12-03 03:43:47 +0200

    day 2

--- day02/part2.fsx
+++ day02/part2.fsx
@@ -8,6 +8,15 @@ open System.IO
 // shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 // outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 
+type Outcome = Lose | Draw | Win
+    with
+    static member parse (c:char) = 
+        match c with 
+        | 'X' -> Lose
+        | 'Y' -> Draw
+        | 'Z' -> Win
+        | _ -> failwith "invalid input"
+
 type Play = Rock | Paper | Scissors
     with 
     static member parse (c:char) = 
@@ -16,6 +25,12 @@ type Play = Rock | Paper | Scissors
         | 'B' | 'Y' -> Paper
         | 'C' | 'Z' -> Scissors
         | _ -> failwith "invalid input"
+
+    static member findPlay (theirs:Play) (outcome:Outcome) : Play =
+        match theirs, outcome with 
+        | Rock, Lose | Paper, Win | Scissors, Draw -> Scissors
+        | Scissors, Lose | Rock, Win | Paper, Draw -> Paper
+        | Paper, Lose | Scissors, Win | Rock, Draw -> Rock
 
     static member score (theirs:Play) (ours:Play) =
         match theirs, ours with 
@@ -35,7 +50,8 @@ __SOURCE_DIRECTORY__ + "/input"
 |> File.ReadLines
 |> Seq.where (String.IsNullOrWhiteSpace >> not)
 |> Seq.map (fun line -> 
-    let round = line[0] |> Play.parse, line[2] |> Play.parse
-    round ||> Play.score
+    let (theirs,outcome) = line[0] |> Play.parse, line[2] |> Outcome.parse
+    let correctPlay = (theirs,outcome) ||> Play.findPlay
+    (theirs,correctPlay) ||> Play.score
     )
 |> Seq.sum
