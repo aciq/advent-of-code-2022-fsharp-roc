b954f8f921be5eb750b5b77348e1137c7ca9af53
aciq <36763595+aciq@users.noreply.github.com>
2022-12-03 05:20:16 +0200

    roc solution day 1

---
+++ watcher.fsx
@@ -0,0 +1,26 @@
+open System.IO
+open System
+open System.Diagnostics
+
+let watcher = new FileSystemWatcher(".","*.roc",EnableRaisingEvents=true, IncludeSubdirectories=true)
+
+let p() = 
+    new Process(StartInfo=ProcessStartInfo(
+            fileName="roc",
+            arguments="check day01/main.roc",
+            WorkingDirectory= __SOURCE_DIRECTORY__ ,
+            RedirectStandardInput=true
+        )
+    )
+       
+let rec loop (nextproctime:DateTimeOffset) : unit =
+    let _ = watcher.WaitForChanged(WatcherChangeTypes.Changed)
+    match DateTimeOffset.Now > nextproctime with 
+    | false -> loop (nextproctime) 
+    | true -> 
+        use p = p()
+        p.Start() |> ignore
+        p.WaitForExit()
+        loop (nextproctime.AddSeconds(2))   
+
+loop DateTimeOffset.Now
